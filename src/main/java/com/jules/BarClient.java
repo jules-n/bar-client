113cf1c78e23de9e8c019846cc3d5c1414c1d68d
jules <neroba.yulia@gmail.com>
2021-12-17 03:47:40 +0200

    Walking skeleton of app

---
+++ src/main/java/com/jules/BarClient.java
@@ -0,0 +1,12 @@
+package com.jules;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarClient {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BarClient.class, args);
+    }
+}
